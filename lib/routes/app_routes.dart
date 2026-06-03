4a399768832f6a838629f960220e151f3d686bf3
Elcio Reis <elcio.reis024@gmail.com>
2026-06-03 17:35:34 -0300

    refactor: adicionar as rotas de edit e login

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,6 +1,8 @@
 class AppRoutes {
   AppRoutes._();
 
+  static final String login = "/login";
   static final String home = "/";
   static final String createCandidate = "/create/candidate";
+  static final String editCandidate = "/edit/candidate";
 }
