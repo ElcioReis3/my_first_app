ba5d86a0d5a3eb62c96fb4d9f1553bbbe1825cda
Elcio Reis <elcio.reis024@gmail.com>
2026-06-03 17:38:53 -0300

    refactor: adicionar nova chamada para o candidateProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:my_first_app/provider/candidate_provider.dart';
+import 'package:provider/provider.dart';
 import 'package:my_first_app/routes/app_router.dart';
 
 void main() {
@@ -10,14 +12,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'App do Elcio',
-      theme: ThemeData(
-        colorScheme: .fromSeed(
-          seedColor: const Color.fromARGB(255, 97, 171, 255),
+    return ChangeNotifierProvider(
+      create: (_) => CandidateProvider(),
+      child: MaterialApp.router(
+        title: 'App do Elcio',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 97, 171, 255),
+          ),
         ),
+        routerConfig: AppRouter.router,
       ),
-      routerConfig: AppRouter.router,
     );
   }
 }
