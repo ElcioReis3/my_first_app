f0c0dd3f65d31b31fb6015a95dfbb140375036bc
Elcio Reis <elcio.reis024@gmail.com>
2026-06-03 17:34:16 -0300

    refactor: adicionar as chamadas para a rota de Login e edit Candidate

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -1,26 +1,39 @@
 import 'package:go_router/go_router.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:my_first_app/models/candidate.dart';
 import 'package:my_first_app/routes/app_routes.dart';
 import 'package:my_first_app/screens/create_candidate_screen.dart';
+import 'package:my_first_app/screens/edit_candidate_screen.dart';
 import 'package:my_first_app/screens/home_screen.dart';
+import 'package:my_first_app/screens/login_screen.dart';
 
 class AppRouter {
   AppRouter._();
 
   static final GoRouter router = GoRouter(
-    initialLocation: AppRoutes.home,
+    initialLocation: AppRoutes.login,
     debugLogDiagnostics: true,
     routes: [
       GoRoute(
-        path: AppRoutes.home,
-        builder: (BuildContext context, GoRouterState state) =>
-            const HomeScreen(),
+        path: AppRoutes.login,
+        builder: (context, state) => const LoginScreen(),
       ),
-
+      GoRoute(
+        path: AppRoutes.home,
+        builder: (context, state) => const HomeScreen(),
+      ),
       GoRoute(
         path: AppRoutes.createCandidate,
-        builder: (BuildContext context, GoRouterState state) =>
-            const CreateCandidateScreen(),
+        builder: (context, state) => const CreateCandidateScreen(),
+      ),
+      GoRoute(
+        path: AppRoutes.editCandidate,
+        builder: (context, state) {
+          final extra = state.extra as Map<String, dynamic>;
+          return EditCandidateScreen(
+            candidateIndex: extra['index'] as int,
+            candidate: extra['candidate'] as Candidate,
+          );
+        },
       ),
     ],
   );
